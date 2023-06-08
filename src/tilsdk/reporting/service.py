til-23/til-23-finals-public

commit aac4fbdbc003b3885ffd69f202ea68d4f740a957

Informs user there are still checkpoints if the robot called check_pose too near the end goal without completing all required checkpoints.

src/tilsdk/reporting/service.py
@@ -200,7 +200,12 @@ class ReportingService:
                 pose_tup = eval(data)
                 return pose_tup
         elif response.status == 300:
-            return 'Not An Expected Checkpoint'
+            if data == 'Not An Expected Checkpoint':
+                return data
+            elif data == "You Still Have Checkpoints":
+                return data
+            else:
+                raise Exception(f"Unhandled Response. Response code: {response.status}.")
         else:
             raise Exception(f"Bad Response from server. Response code: {response.status}. " + 
                             f"Try checking that inputs are correct or that the server is up.")

src/tilscoring/server.py
@@ -194,17 +194,20 @@ def get_check_pose():
         # if pose_counter > len(valid_pose):
         #     return 'Please exit the maze using the last location given',300
         
-        if euclidean_distance(pose, valid_pose[-1]) < config['local_thres']:
-            checkpoint_state = CheckpointState.VALID_CHECKPOINT
-            score=0
+        if euclidean_distance(pose, valid_pose[-1]) < config['local_thres']:  # near end goal.
+            if pose_counter < len(valid_pose - 1):
+                return "You Still Have Checkpoints", 300
+            else:
+                checkpoint_state = CheckpointState.VALID_CHECKPOINT
+                score=0
 
-            status_dict = {
-                'status':  'goal reached',
-                'time_elapsed': time_elapsed_s
-            }
-            announcer.announce(event='status update', data_dict=status_dict)
+                status_dict = {
+                    'status':  'goal reached',
+                    'time_elapsed': time_elapsed_s
+                }
+                announcer.announce(event='status update', data_dict=status_dict)
 
-            return 'End Goal Reached', 200
+                return 'End Goal Reached', 200
         elif euclidean_distance(pose, valid) < config['local_thres']:
             checkpoint_state = CheckpointState.VALID_CHECKPOINT
             

commit 2b172af971c8842034a198dab9fe63f2d95eb208

fix data being undefined
if response code is 300, data is not being defined like in response code = 200, causing NameError (regression from https://github.com/til-23/til-23-finals-public/commit/aac4fbdbc003b3885ffd69f202ea68d4f740a957)

src/tilsdk/reporting/service.py
@@ -200,6 +200,7 @@ class ReportingService:
                 pose_tup = eval(data)
                 return pose_tup
         elif response.status == 300:
+            data = response.data.decode('utf-8')
             if data == 'Not An Expected Checkpoint':
                 return data
             elif data == "You Still Have Checkpoints":